038a049780bb99277a9f69e3d8b614b32d0cf260
IlyaShelestov <shelestov_ilya2905@mail.ru>
2025-02-01 21:20:17 +0500

    Format code

--- backend/tests/integration/authRoutes.test.js
+++ backend/tests/integration/authRoutes.test.js
@@ -3,40 +3,40 @@ const server = require("../../src/index");
 const pool = require("../../src/config/db");
 
 describe("Auth API Tests", () => {
-    beforeAll(async () => {
-        await pool.query(`SELECT 1`);
-    })
+  beforeAll(async () => {
+    await pool.query(`SELECT 1`);
+  });
 
-    afterAll(async () => {
-        try {
-            await pool.query("DELETE FROM users WHERE iin = $1", ["123456789012"]);
-            await pool.end();
-            server.close();
-        } catch (err) {
-            console.log("Error during cleanup:", err);
-            throw err;
-        }
-    })
+  afterAll(async () => {
+    try {
+      await pool.query("DELETE FROM users WHERE iin = $1", ["123456789012"]);
+      await pool.end();
+      server.close();
+    } catch (err) {
+      console.log("Error during cleanup:", err);
+      throw err;
+    }
+  });
 
-    test('Should register a user', async () => {
-        const res = await request(server).post('/api/auth/register').send({
-            iin: '123456789012',
-            name: 'TestName',
-            surname: 'TestSurname',
-            patronymic: 'TestPatronymic',
-            nationality: 'TestNationality',
-            birth_date: '2021-01-01',
-            region: 'TestRegion',
-            city: 'TestCity',
-            address: 'TestAddress',
-            sex: 'TestSex',
-            email: "test@test.com",
-            phone: '1234567890',
-            password: "testpassword",
-            role: "user",
-        })
+  test("Should register a user", async () => {
+    const res = await request(server).post("/api/auth/register").send({
+      iin: "123456789012",
+      name: "TestName",
+      surname: "TestSurname",
+      patronymic: "TestPatronymic",
+      nationality: "TestNationality",
+      birth_date: "2021-01-01",
+      region: "TestRegion",
+      city: "TestCity",
+      address: "TestAddress",
+      sex: "TestSex",
+      email: "test@test.com",
+      phone: "1234567890",
+      password: "testpassword",
+      role: "user",
+    });
 
-        expect(res.status).toBe(201);
-        expect(res.body[0]).toHaveProperty('iin', '123456789012');
-    })
-})
+    expect(res.status).toBe(201);
+    expect(res.body[0]).toHaveProperty("iin", "123456789012");
+  });
+});
