16c1b0accb5a99b9bc2e8df8380a54669fc1670f
sSunR1sEe <RA1esentaev@gmail.com>
2025-02-07 23:55:24 +0500

    Create logic of data validation in dataValidation

---
+++ backend/src/utils/dataValidation.js
@@ -0,0 +1,57 @@
+const validator = require("validator");
+
+const isValidName = (name, min = 2, max = 20) => {
+  return (
+    typeof name === "string" &&
+    name.length >= min &&
+    name.length <= max &&
+    /^[А-ЯЁ][а-яё]+(-[А-ЯЁ][а-яё]+)?$/.test(name) // Разрешены только русские буквы, первая буква заглавная, двойные имена (Касым-Жомарт)
+  );
+};
+
+const isValidSurname = (name, min = 2, max = 20) => {
+  return (
+    typeof name === "string" &&
+    name.length >= min &&
+    name.length <= max &&
+    /^[А-ЯЁ][а-яё]+$/.test(name) // Разрешены только русские буквы, первая буква заглавная
+  );
+};
+
+const isValidIIN = (iin) => {
+  return typeof iin === "string" && /^[0-9]{12}$/.test(iin); // Разрешены только цифры, длина должна быть равна 12 символам
+};
+
+const isValidText = (text, min = 1, max = 255) => {
+  return (
+    typeof text === "string" &&
+    text.length >= min &&
+    text.length <= max &&
+    /^[a-zA-Zа-яёА-ЯЁ0-9\s.,!?()-]+$/.test(text) // Разрешены буквы на кириллице и латинице, также пробел и специальные символы .,!?()-
+  );
+};
+
+const isValidEmail = (email) => {
+  return typeof email === "string" && validator.isEmail(email);
+};
+
+const isValidDate = (date) => {
+  return (
+    typeof date === "string" &&
+    validator.isDate(date, { format: "YYYY-MM-DD", strictMode: true })
+  );
+};
+
+const isValidPhoneNumber = (phone, locale = "kz-KZ") => {
+  return typeof phone === "string" && validator.isMobilePhone(phone, locale);
+};
+
+module.exports = {
+  isValidName,
+  isValidSurname,
+  isValidIIN,
+  isValidText,
+  isValidEmail,
+  isValidDate,
+  isValidNumber,
+};
