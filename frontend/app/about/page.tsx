0796ab671e789449c566631a07acdeb34d256054
s3r1msultan <ersultanserimbetov@gmail.com>
2025-02-20 02:49:32 +0500

    created about us page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
+import "./AboutUs.scss";
 export default function About() {
+  const profiles = [
+    {
+      name: "Антон Птушкин",
+      image: "/images/ornament.png",
+      role: "Frontend Developer",
+    },
+    {
+      name: "Андрей Шевченко",
+      image: "/images/ornament.png",
+      role: "Backend Developer",
+    },
+    {
+      name: "Генадий Головкин",
+      image: "/images/ornament.png",
+      role: "Cybersecurity specialist",
+    },
+  ];
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <h1>О нас</h1>
+      <div className="about_us">
+        {profiles.map((profile) => (
+          <div
+            className="profile"
+            key={profile.name}
+          >
+            <Image
+              src={profile.image}
+              alt={profile.name}
+              width={275}
+              height={275}
+            />
+            <h2>{profile.name}</h2>
+            <p>{profile.role}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
