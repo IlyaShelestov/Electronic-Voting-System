c0d748f0f3ff69361906cb85223883201786eb4b
IlyaShelestov <shelestov_ilya2905@mail.ru>
2025-06-08 18:49:43 +0500

    fix: change populate data in the egov service

--- services/egov/scripts/populateData.js
+++ services/egov/scripts/populateData.js
@@ -4,34 +4,41 @@ const pool = require("../src/config/db");
 async function populateData() {
   try {
     await pool.query("BEGIN");
+
+    console.log("Populating citizens data...");
+
+    // Admin and Manager
+    let citizenValues = `
+      ('111111111111', '+77220000000', 'admin@example.com'),
+      ('222222222222', '+77220000001', 'manager@example.com')`;
+
+    // 12 Candidates
+    for (let i = 0; i < 12; i++) {
+      const iin = `3${i.toString().padStart(11, "0")}`;
+      citizenValues += `,
+      ('${iin}', '+7701000${(i + 100)
+        .toString()
+        .padStart(4, "0")}', 'candidate${i + 1}@example.com')`;
+    }
+
+    // 100 Regular users
+    for (let i = 0; i < 100; i++) {
+      const iin = `9${i.toString().padStart(11, "0")}`;
+      citizenValues += `,
+      ('${iin}', '+7701${i.toString().padStart(7, "0")}', 'user${
+        i + 1
+      }@example.com')`;
+    }
+
     await pool.query(`
-                INSERT INTO citizens (iin, phone_number, email)
-                VALUES
-                ('111111111111', '+77010000000', 'admin@example.com'),
-                ('222222222222', '+77010000001', 'manager@example.com'),
-                ('333333333333', '+77010000002', 'ivan@example.com'),
-                ('444444444444', '+77010000003', 'alexey@example.com'),
-                ('555555555555', '+77010000004', 'maria@example.com'),
-                ('666666666666', '+77010000005', 'sergey@example.com'),
-                ('777777777777', '+77010000006', 'nikolay@example.com'),
-                ('888888888888', '+77010000007', 'petr@example.com'),
-                ('999999999999', '+77010000008', 'dmitry@example.com'),
-                ('121212121212', '+77010000009', 'andrey@example.com'),
-                ('131313131313', '+77010000010', 'konstantin@example.com'),
-                ('141414141414', '+77010000011', 'olga@example.com'),
-                ('151515151515', '+77010000012', 'ekaterina@example.com'),
-                ('161616161616', '+77010000013', 'vasiliy@example.com'),
-                ('171717171717', '+77010000014', 'tatiana@example.com'),
-                ('181818181818', '+77010000015', 'user11@example.com'),
-                ('191919191919', '+77010000016', 'user12@example.com'),
-                ('202020202020', '+77010000017', 'user13@example.com'),
-                ('212121212121', '+77010000018', 'user14@example.com'),
-                ('222222222223', '+77010000019', 'user15@example.com');
-            `);
+      INSERT INTO citizens (iin, phone_number, email)
+      VALUES ${citizenValues}
+    `);
+
     await pool.query("COMMIT");
-    console.log("Data populated successfully.");
+    console.log("Citizens data populated successfully.");
   } catch (err) {
-    pool.query("ROLLBACK");
+    await pool.query("ROLLBACK");
     console.error("Error populating data:", err);
   } finally {
     pool.end();
